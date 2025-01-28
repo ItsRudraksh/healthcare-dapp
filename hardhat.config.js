bc4fcf8318100fe44553586a38c4dca2ade372e4
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2025-01-29 02:32:41 +0530

    healtcare-dapp-first

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_RPC_URL = "https://rpc.ankr.com/eth_holesky";
+const NEXT_PUBLIC_PRIVATE_KEY =
+  "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
+  networks: {
+    besu_local: {
+      url: "http://localhost:8545", // Besu RPC URL (or your custom one)
+      accounts: [
+        `0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3`,
+      ], // Your wallet private key (make sure it's in an .env file)
+    },
+    localhost: {
+      chainId: 1337,
+      url: "http://127.0.0.1:8545", // Besu local RPC (if that's where Besu is running)
+      accounts: [
+        `0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3`,
+      ],
+    },
+  },
+};
